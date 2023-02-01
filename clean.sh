b8ec8d8e3b73840e8448014066baf71b2596079f
Mark Raynsford <code@io7m.com>
2023-02-01 16:35:06 +0000

    Update images

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@
 
 for p in $(cat projects.txt)
 do
-  ./clean-one.sh "${p}"
+  ./clean-one.sh "${p}" || true
 done
--- looseleaf/make.sh
+++ looseleaf/make.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
 VERSION=$(head -n 1 version.txt)
+VERSION_ALPINE=$(head -n 1 version_alpine.txt)
+VERSION_JDK=$(head -n 1 version_jdk.txt)
 
 wget -c -O dist.zip "https://repo1.maven.org/maven2/com/io7m/looseleaf/com.io7m.looseleaf.cmdline/${VERSION}/com.io7m.looseleaf.cmdline-${VERSION}-distribution.zip"
 wget -c -O dist.zip.asc "https://repo1.maven.org/maven2/com/io7m/looseleaf/com.io7m.looseleaf.cmdline/${VERSION}/com.io7m.looseleaf.cmdline-${VERSION}-distribution.zip.asc"
@@ -9,4 +11,7 @@ unzip -o dist.zip
 
 exec docker build \
 --build-arg "version=${VERSION}" \
+--build-arg "version_alpine=${VERSION_ALPINE}" \
+--build-arg "version_jdk=${VERSION_JDK}" \
+--iidfile image-id.txt \
 -t "io7m/looseleaf:${VERSION}" .
