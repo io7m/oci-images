47c0fbd6a095a95db9a50e7c370845eb5c9a9657
Mark Raynsford <code@io7m.com>
2023-02-01 16:45:13 +0000

    Update postservice

--- postservice/make.sh
+++ postservice/make.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -ex
 
 VERSION=$(head -n 1 version.txt)
+VERSION_ALPINE=$(head -n 1 version_alpine.txt)
 
 exec docker build \
 --build-arg "version=${VERSION}" \
+--build-arg "version_alpine=${VERSION_ALPINE}" \
+--iidfile image-id.txt \
 -t "io7m/postservice:${VERSION}" .
