4516176012ee1860931be76e22f004336fae099d
Mark Raynsford <code@io7m.com>
2023-04-23 08:18:49 +0000

    Move versions to files

--- idstore/make.sh
+++ idstore/make.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
 VERSION=$(head -n 1 version.txt)
+VERSION_ALPINE=$(head -n 1 version_alpine.txt)
+VERSION_JDK=$(head -n 1 version_jdk.txt)
 
 wget -c -O dist.zip "https://repo1.maven.org/maven2/com/io7m/idstore/com.io7m.idstore.server.main/${VERSION}/com.io7m.idstore.server.main-${VERSION}-distribution.zip"
 wget -c -O dist.zip.asc "https://repo1.maven.org/maven2/com/io7m/idstore/com.io7m.idstore.server.main/${VERSION}/com.io7m.idstore.server.main-${VERSION}-distribution.zip.asc"
@@ -9,4 +11,6 @@ unzip -o dist.zip
 
 exec docker build \
 --build-arg "version=${VERSION}" \
+--build-arg "version_alpine=${VERSION_ALPINE}" \
+--build-arg "version_jdk=${VERSION_JDK}" \
 -t "io7m/idstore:${VERSION}" .
